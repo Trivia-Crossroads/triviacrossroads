288eb07b57df5890d1b325f1915ac8b078129b9c
David Tolmie <dave@bodhitech.com>
2015-11-16 13:35:51 -0800

    Added Firebase code examples to FirebaseExamples.js

---
+++ FirebaseExamples.js
@@ -0,0 +1,23 @@
+// Example of referencing the Person DB in Firebase
+var fbPerson = new Firebase('https://trivia-crossroads.firebaseio.com/Person');
+
+//Example of pulling the objects from Person
+fbPerson.on("value", function(snapshot) {
+  console.log(snapshot.val());
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+
+//Example of pushing new content to People
+//Do NOT use .set, as it will overwrite all existing data with what you're setting.
+fbPerson.push({name: 'Selena', gender: 'female'});
+
+// By setting the URL to a specific FB UUID, we can access that object directly
+//var fbPerson = new Firebase('https://trivia-crossroads.firebaseio.com/Person/-K3H95UMwdDSzN0ZbH9d');
+
+// Here's an example of filtering by key/value when querying FB
+fbPerson.orderByChild("gender").equalTo('male').on("value", function(snapshot) {
+  console.log(snapshot.val());
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
