1581764b9b6e19aba6cf6c9b8971825ad7b59954
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2015-11-16 11:56:00 -0800

    Created object constructor for new profile and for existing profile data

---
+++ people.js
@@ -0,0 +1,8 @@
+function ExistingProfiles(name, email, neighborhood, day, specialty){
+  this.name = name;
+  this.email = email;
+  this.neighborhood = neighborhood;
+  this.day = day;
+  this.specialty = specialty;
+}
+
