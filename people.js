8c08684af587a8f0ade0b8cdd06de1282b620efb
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2015-11-17 08:58:08 -0800

    removed extra array

--- people.js
+++ people.js
@@ -1,18 +1,17 @@
 var fbPerson = new Firebase('https://trivia-crossroads.firebaseio.com/Person');
 var profileList = [];
-var parsedProfileList = [];
 
 fbPerson.once("value", function(snapshot) {
   snapshot.forEach(function(childSnapshot) {
-    parsedProfileList.push(JSON.parse(childSnapshot.val()));
+    profileList.push(JSON.parse(childSnapshot.val()));
   });
   var table = document.getElementById('profileDisplay');
-  for(var i = 0; i < parsedProfileList.length; i++){
+  for(var i = 0; i < profileList.length; i++){
     var tr = document.createElement('tr');
 
-    for(var j in parsedProfileList[i]){
+    for(var j in profileList[i]){
       var td = document.createElement('td');
-      td.textContent = parsedProfileList[i][j];
+      td.textContent = profileList[i][j];
       tr.appendChild(td);
     }
   table.appendChild(tr);
