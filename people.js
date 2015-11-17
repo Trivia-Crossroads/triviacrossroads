16e019a5400babf1a854242c5ac3682da91554d9
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2015-11-17 13:26:00 -0800

    generate new tbody for every search

--- people.js
+++ people.js
@@ -1,23 +1,37 @@
 var fbPerson = new Firebase('https://trivia-crossroads.firebaseio.com/Person');
 var profileList = [];
+var timeDrop = document.getElementById('time');
 
-function generateTable(){
+function generateTable(dataArray){
   var table = document.getElementById('profileDisplay');
-  for(var i = 0; i < profileList.length; i++){
+  var tbody = document.createElement('tbody');
+  for(var i = 0; i < dataArray.length; i++){
     var tr = document.createElement('tr');
 
-    for(var j in profileList[i]){
+    for(var j in dataArray[i]){
       var td = document.createElement('td');
-      td.textContent = profileList[i][j];
+      td.textContent = dataArray[i][j];
       tr.appendChild(td);
     }
-  table.appendChild(tr);
+  tbody.appendChild(tr);
   }
+  table.appendChild(tbody);
+  table.removeChild(tbody.previousSibling);
 }
+
+function sortDay(e){
+  var pickedDay = timeDrop.options[timeDrop.selectedIndex].text;
+  sortArray = profileList.filter(function av(v){return v.day === pickedDay;});
+  generateTable(sortArray);
+}
+
 
 fbPerson.once("value", function(snapshot) {
   snapshot.forEach(function(childSnapshot) {
     profileList.push(JSON.parse(childSnapshot.val()));
   });
-  generateTable();
+  var sortArray = [];
+  generateTable(profileList);
+  timeDrop.addEventListener('change', sortDay);
 });
+
