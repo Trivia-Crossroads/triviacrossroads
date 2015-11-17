c084411fab98696ab0161c54ca3c062006236209
Michael Sullivan <sapientia.veritas@gmail.com>
2015-11-17 11:17:38 -0800

    added functionality to display random trivia fact on landing page

---
+++ index.js
@@ -0,0 +1,19 @@
+randomTrivia = ["Did you know? The word \"trivia\" comes from the Latin word \"trivium\", meaning a meeting of three roads.", "In the Middle Ages the \"trivium\" meant the curriculum of study comprising grammar, logic, and rhetoric.",
+"Oedipus killed his father at a trivium, a meeting of three roads. He later defeated the Sphinx in a trivia contest and saved the city of Thebes! Then he married his mother."];
+
+displayRandom = {
+  randomNum: function () {
+    return Math.floor(Math.random() * randomTrivia.length);
+    console.log(randomNum);
+  },
+
+  randomFact: function () {
+    num = displayRandom.randomNum();
+    return randomTrivia[num];
+    console.log(randomFact);
+  },
+
+}
+
+container = document.getElementById("container");
+container.textContent = displayRandom.randomFact();
