58e8f2b789b6f076b7c436ca0be4dd1bbe95667e
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2015-11-17 08:53:42 -0800

    removed object constructer. Was not using it.

--- people.js
+++ people.js
@@ -1,13 +1,4 @@
 var fbPerson = new Firebase('https://trivia-crossroads.firebaseio.com/Person');
-
-function ExistingProfiles(name, email, neighborhood, day, specialty){
-  this.name = name;
-  this.email = email;
-  this.neighborhood = neighborhood;
-  this.day = day;
-  this.specialty = specialty;
-}
-
 var profileList = [];
 var parsedProfileList = [];
 
