a3f2bbcd6e43e36d562730921f19543fb49357ce
Michael Sullivan <sapientia.veritas@gmail.com>
2015-11-18 11:31:58 -0800

    moved all the functions into an object

--- people.js
+++ people.js
@@ -5,87 +5,88 @@ var specialtyDrop = document.getElementById('specialty');
 var locationDrop = document.getElementById('locations');
 var toggle = document.getElementById('toggle');
 
+var peopleTable = {
+   generateTable: function(dataArray){
+    var table = document.getElementById('profileDisplay');
+    var tbody = document.createElement('tbody');
+    for(var i = 0; i < dataArray.length; i++){
+      var tr = document.createElement('tr');
 
-function generateTable(dataArray){
-  var table = document.getElementById('profileDisplay');
-  var tbody = document.createElement('tbody');
-  for(var i = 0; i < dataArray.length; i++){
-    var tr = document.createElement('tr');
-
-    for(var j in dataArray[i]){
-      var td = document.createElement('td');
-      td.textContent = dataArray[i][j];
-      tr.appendChild(td);
+      for(var j in dataArray[i]){
+        var td = document.createElement('td');
+        td.textContent = dataArray[i][j];
+        tr.appendChild(td);
+      }
+    tbody.appendChild(tr);
     }
-  tbody.appendChild(tr);
-  }
-  table.appendChild(tbody);
-  table.removeChild(tbody.previousSibling);
-}
+    table.appendChild(tbody);
+    table.removeChild(tbody.previousSibling);
+  },
 
-function sortDay(e){
-  var pickedDay = dayDrop.options[dayDrop.selectedIndex].text;
-  if (pickedDay == "--"){
-    sortArray = profileList;
-  } else {
-    sortArray = profileList.filter(function av(v){return v.day === pickedDay;});
-  }
-  generateTable(sortArray);
-}
+   sortDay: function(e){
+    var pickedDay = dayDrop.options[dayDrop.selectedIndex].text;
+    if (pickedDay == "--"){
+      sortArray = profileList;
+    } else {
+      sortArray = profileList.filter(function av(v){return v.day === pickedDay;});
+    }
+    peopleTable.generateTable(sortArray);
+  },
 
-function sortSpecialty(e){
-  var pickedSpecialty = specialtyDrop.options[specialtyDrop.selectedIndex].text;
-  if (pickedSpecialty === 'Sorcery, Phrenology, and Chiromancy') {
-    pickedSpecialty = 'Sorcery';
-  }
-  if (pickedSpecialty == "--"){
-    sortArray = profileList;
-  } else {
-    sortArray = profileList.filter(function av(v){return v.specialty === pickedSpecialty;});
-  }
-  generateTable(sortArray);
-}
+   sortSpecialty: function(e){
+    var pickedSpecialty = specialtyDrop.options[specialtyDrop.selectedIndex].text;
+    if (pickedSpecialty === 'Sorcery, Phrenology, and Chiromancy') {
+      pickedSpecialty = 'Sorcery';
+    }
+    if (pickedSpecialty == "--"){
+      sortArray = profileList;
+    } else {
+      sortArray = profileList.filter(function av(v){return v.specialty === pickedSpecialty;});
+    }
+    peopleTable.generateTable(sortArray);
+  },
 
-function sortLocation(e){
-  var pickedLocation = locationDrop.options[locationDrop.selectedIndex].text;
-  if (pickedLocation == "--"){
-    sortArray = profileList;
-  } else {
-    sortArray = profileList.filter(function av(v){return v.location === pickedLocation;});
-  }
-  generateTable(sortArray);
-}
+   sortLocation: function(e){
+    var pickedLocation = locationDrop.options[locationDrop.selectedIndex].text;
+    if (pickedLocation == "--"){
+      sortArray = profileList;
+    } else {
+      sortArray = profileList.filter(function av(v){return v.location === pickedLocation;});
+    }
+    peopleTable.generateTable(sortArray);
+  },
 
-function storagePull(){
-  fbPerson.once("value", function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-      profileList.push(JSON.parse(childSnapshot.val()));
+   storagePull: function(){
+    fbPerson.once("value", function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        profileList.push(JSON.parse(childSnapshot.val()));
+      });
+      peopleTable.generateTable(profileList);
+      var sortArray = [];
+      dayDrop.addEventListener('change', peopleTable.sortDay);
+      specialtyDrop.addEventListener('change', peopleTable.sortSpecialty);
+      locationDrop.addEventListener('change', peopleTable.sortLocation);
     });
-    generateTable(profileList);
-    var sortArray = [];
-    dayDrop.addEventListener('change', sortDay);
-    specialtyDrop.addEventListener('change', sortSpecialty);
-    locationDrop.addEventListener('change', sortLocation);
-  });
-}
+  },
 
-function togglePeople (event){
-  if (event.target.id == 'dayButton') {
-   dayDrop.removeAttribute('class', 'hidden');
-    specialtyDrop.setAttribute('class', 'hidden');
-    locationDrop.setAttribute('class', 'hidden');
-  } else if (event.target.id == 'specialtyButton') {
-  specialtyDrop.removeAttribute('class', 'hidden');
-    dayDrop.setAttribute('class', 'hidden');
-    locationDrop.setAttribute('class', 'hidden');
-  } else if (event.target.id == 'locationButton') {
-  locationDrop.removeAttribute('class', 'hidden');
-    dayDrop.setAttribute('class', 'hidden');
-    specialtyDrop.setAttribute('class', 'hidden');
+   togglePeople: function(event){
+    if (event.target.id == 'dayButton') {
+     dayDrop.removeAttribute('class', 'hidden');
+      specialtyDrop.setAttribute('class', 'hidden');
+      locationDrop.setAttribute('class', 'hidden');
+    } else if (event.target.id == 'specialtyButton') {
+    specialtyDrop.removeAttribute('class', 'hidden');
+      dayDrop.setAttribute('class', 'hidden');
+      locationDrop.setAttribute('class', 'hidden');
+    } else if (event.target.id == 'locationButton') {
+    locationDrop.removeAttribute('class', 'hidden');
+      dayDrop.setAttribute('class', 'hidden');
+      specialtyDrop.setAttribute('class', 'hidden');
+    }
+
   }
-
 }
 
-storagePull();
-toggle.addEventListener('click', togglePeople);
+peopleTable.storagePull();
+toggle.addEventListener('click', peopleTable.togglePeople);
 
